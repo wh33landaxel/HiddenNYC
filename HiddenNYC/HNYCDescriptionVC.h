85a911f22d08a4f2051fd25bac96ad995c902b68
wh33landaxel <xlnunez317@gmail.com>
2013-01-07 23:10:41 -0500

    second commit

---
+++ HiddenNYC/HNYCDescriptionVC.h
@@ -0,0 +1,17 @@
+//
+//  HNYCDescriptionVC.h
+//  HiddenNYC
+//
+//  Created by Axel Nunez on 1/4/13.
+//  Copyright (c) 2013 CISDD.axel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNYCDescriptionVC : UIViewController
+
+-(void)setDescription:(NSString *) description;
+-(void)setPlaces:(NSDictionary *) dictionary;
+
+
+@end
---
+++ HiddenNYC/HNYCDescriptionVC.m
@@ -0,0 +1,68 @@
+//
+//  HNYCDescriptionVC.m
+//  HiddenNYC
+//
+//  Created by Axel Nunez on 1/4/13.
+//  Copyright (c) 2013 CISDD.axel. All rights reserved.
+//
+
+#import "HNYCDescriptionVC.h"
+#import "LocationBrain.h"
+
+@interface HNYCDescriptionVC ()
+
+@property(weak,nonatomic) NSString *description;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionText;
+@property(strong,nonatomic) NSDictionary * places;
+
+@end
+
+@implementation HNYCDescriptionVC
+@synthesize descriptionText = _descriptionText;
+@synthesize places = _places;
+@synthesize description = _description;
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+-(void)setPlaces:(NSDictionary *)dictionary{
+    if(!_places){
+        _places = [[NSDictionary alloc] initWithDictionary:dictionary];
+        NSLog(@"%@", _places);
+    }
+    else
+        return;
+}
+
+
+
+
+-(void)setDescription:(NSString *)description{
+    _description = description;
+}
+
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _descriptionText.text = _description;
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+}
+
+@end
